3ce0c20e952699a91658d92c7f67bbb9a8c50364
Eitan Barylko <eitan@barylko.com>
2023-12-25 01:04:14 -0800

    Modified unique_substring

--- python/optimal_partition/optimal_partition.py
+++ python/optimal_partition/optimal_partition.py
@@ -11,13 +11,13 @@ def unique_substring(substrings, to_process):
     """
     chars_consumed = set()
 
-    current_char, rest = [itz.first(to_process), itz.drop(1, to_process)]
+    current_char, rest = [itz.first(to_process), to_process]
     # return [current_char, list(rest)]
     substring = ""
     while current_char and current_char not in chars_consumed:
         chars_consumed.add(current_char)
         substring += str(current_char)
-        rest = list(itz.drop(1, to_process))
+        rest = list(itz.drop(1, rest))
         current_char = itz.first(itz.take(1, rest))
     substrings.append(substring)
     return [substrings, rest]
--- python/optimal_partition/test_optimal_partition.py
+++ python/optimal_partition/test_optimal_partition.py
@@ -6,7 +6,9 @@ import optimal_partition as pt
 
 def test_unique_substring():
     # assert pt.unique_substring([], "s") == [["s"], []]
-    assert pt.unique_substring([], "ss") == [["s"], ["s"]]
+    # assert pt.unique_substring([], "ss") == [["s"], ["s"]]
+    assert pt.unique_substring([], "sts") == [["st"], ["s"]]
+
 
 # def test_optimal_partition():
 #     assert optimal_partition("abacaba") == 4
