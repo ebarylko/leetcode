84e144221d8e7f1b879dd1df7952a4dc7ce968ce
Eitan Barylko <eitan@barylko.com>
2023-12-27 20:44:51 -0800

    Added variant of optimal_partitions

--- python/optimal_partition/optimal_partition.py
+++ python/optimal_partition/optimal_partition.py
@@ -29,15 +29,23 @@ def unique_substring(tuple):
     return substrings, rest
 
 
+# def unique_substrings(word):
+#     # return tz.pipe(
+#     #     [[], list(word)],
+#     #     ft.partial(itz.iterate, unique_substring),
+#     #     ft.partial(it.dropwhile, operator.itemgetter(1)),
+#     #     next,
+#     #     operator.itemgetter(0)
+# )
+
+
 def unique_substrings(word):
-    return tz.pipe(
-        [[], list(word)],
-        ft.partial(itz.iterate, unique_substring),
-        ft.partial(it.dropwhile, operator.itemgetter(1)),
-        next,
-        operator.itemgetter(0)
-        # list
-    )
+    substrings, to_process = [[], list(word)]
+
+    while to_process:
+        substrings, to_process = unique_substring((substrings, to_process))
+
+    return substrings
 
 
 def optimal_partition(word):
@@ -46,4 +54,37 @@ def optimal_partition(word):
     :return: the minimum number of substrings made by partitioning the word that
     contain unique characters
     """
-    return unique_substrings(word)
+    return len(unique_substrings(word))
+
+
+def unique_substring_2(to_process):
+    """
+    :param to_process: the rest of the original word which
+    has not been analyzed for unique substrings
+    :return: what remains of the original word to analyze after finding the latest partition
+    ex: unique_substring_2("aba") -> "a"
+    """
+    chars_consumed = set()
+
+    rest = to_process
+    while rest and rest[0] not in chars_consumed:
+        current_char, *rest = rest
+        chars_consumed.add(current_char)
+
+    return rest
+
+def optimal_partition_2(word):
+    """
+    :param word: the word to partition
+    :return: the minimum number of substrings made by partitioning the word that
+    contain unique characters
+    """
+    to_process = list(word)
+    number_of_substrings = 0
+
+    while to_process:
+        to_process = unique_substring_2(to_process)
+        number_of_substrings += 1
+
+    return number_of_substrings
+
--- python/optimal_partition/test_optimal_partition.py
+++ python/optimal_partition/test_optimal_partition.py
@@ -15,8 +15,14 @@ def test_unique_substring():
 
 
 def test_unique_substrings():
-    assert pt.unique_substrings("abacaba") == 2
+    assert pt.unique_substrings("abacaba") == ['ab', 'ac', 'ab', 'a']
+    assert pt.unique_substrings("abc") == ['abc']
+    assert pt.unique_substrings("ssssss") == ['s', 's', 's', 's', 's', 's']
 
-# def test_optimal_partition():
-#     assert optimal_partition("abacaba") == 4
-#     assert optimal_partition("ssssss") == 6
+
+
+def test_optimal_partition():
+    assert pt.optimal_partition("abacaba") == 4
+    assert pt.optimal_partition("ssssss") == 6
+    assert pt.optimal_partition_2("abacaba") == 4
+    assert pt.optimal_partition_2("ssssss") == 6
