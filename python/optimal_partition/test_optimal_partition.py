0643959d646b764cab47b0f6e955159b2499c179
Eitan Barylko <eitan@barylko.com>
2024-01-01 19:15:57 -0800

    Modified the tests

--- python/optimal_partition/test_optimal_partition.py
+++ python/optimal_partition/test_optimal_partition.py
@@ -1,15 +1,12 @@
 import optimal_partition as pt
 
 
-# def test_simple_things():
-#     assert 1
-
 def test_unique_substring():
-    # assert pt.unique_substring([], "") == [[""], ""]
-    # assert pt.unique_substring([], "s") == [["s"], []]
-    # assert pt.unique_substring([], "ss") == [["s"], ["s"]]
-    # assert pt.unique_substring([], "sts") == [["st"], ["s"]]
-    # assert pt.unique_substring([], "stq") == [["stq"], []]
+    assert pt.unique_substring(([], "")) == ([""], "")
+    assert pt.unique_substring(([], "s")) == (["s"], [])
+    assert pt.unique_substring(([], "ss")) == (["s"], ["s"])
+    assert pt.unique_substring(([], "sts")) == (["st"], ["s"])
+    assert pt.unique_substring(([], "stq")) == (["stq"], [])
     assert pt.unique_substring(([], "abacaba")) == (["ab"], ['a', 'c', 'a', 'b', 'a'])
     assert pt.unique_substring((["ab"], ['a', 'c', 'a', 'b', 'a'])) == (["ab", "ac"], ['a', 'b', 'a'])
 
@@ -18,7 +15,6 @@ def test_unique_substrings():
     assert pt.unique_substrings("abacaba") == ['ab', 'ac', 'ab', 'a']
     assert pt.unique_substrings("abc") == ['abc']
     assert pt.unique_substrings("ssssss") == ['s', 's', 's', 's', 's', 's']
-
 
 
 def test_optimal_partition():
