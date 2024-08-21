3030593aab42edb272247ce2cf00c2617f16f9bf
Eitan Barylko <eitan@barylko.com>
2024-08-20 20:24:33 -0700

    Added more tests

--- python/roman-to-integer/roman_to_integer.py
+++ python/roman-to-integer/roman_to_integer.py
@@ -1,5 +1,6 @@
 from functools import reduce
-from toolz import concat, first, second, juxt, thread_last
+from itertools import dropwhile
+from toolz import concat, first, second, juxt, thread_last, iterate, take
 
 
 numerals_to_integer = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
@@ -24,19 +25,33 @@ def extract_portion_to_eval(roman_numeral: str) -> str:
     )
 
 
-# def split_into_expanded_form(numeral: str) -> list[str]:
-#     """
-#     :param numeral: a roman numeral
-#     :return: the portions of the given numeral to evaluate
-#     when determining the numerical value
-#     """
-#     def add_portion_of_numeral_to_eval(to_evaluate: list[str], remaining_numeral: str):
-#         if len(str) == 1:
-#             return concat([to_evaluate, remaining_numeral])
-#
-#         portion = extract_portion(remaining_numeral)
-#
-#     return reduce()
+def split_into_expanded_form(numeral: str) -> list[str]:
+    """
+    :param numeral: a roman numeral
+    :return: the portions of the given numeral to evaluate
+    when determining the numerical value
+    """
+    def on_last_portion(roman_numeral):
+        return extract_portion_to_eval(roman_numeral) == roman_numeral
+
+    def add_portion_of_numeral_to_eval(remaining_numeral_and_portions_to_eval: tuple[list[str], str]):
+        expanded_form, remaining_numeral = remaining_numeral_and_portions_to_eval
+        if on_last_portion(numeral):
+            return expanded_form + [remaining_numeral], []
+        else:
+            portion_to_eval = extract_portion_to_eval(remaining_numeral)
+            return expanded_form + [portion_to_eval], remaining_numeral[len(portion_to_eval):]
+
+    def still_processing_numeral(coll):
+        _, remaining_numeral = coll
+        return len(remaining_numeral) != 0
+
+    return thread_last(([], numeral),
+                       (iterate, add_portion_of_numeral_to_eval),
+                       (dropwhile, still_processing_numeral),
+                       first,
+                       first)
+
 
 def numeral_to_integer(numeral: str) -> int:
     """
--- python/roman-to-integer/test_roman_to_integer.py
+++ python/roman-to-integer/test_roman_to_integer.py
@@ -3,15 +3,19 @@ import roman_to_integer as ri
 
 def test_extract_portion_to_eval():
     assert ri.extract_portion_to_eval("III") == "I"
+    assert ri.extract_portion_to_eval("IV") == "IV"
     assert ri.extract_portion_to_eval("XL") == "XL"
     assert ri.extract_portion_to_eval("CM") == "CM"
     assert ri.extract_portion_to_eval("MC") == "M"
+    assert ri.extract_portion_to_eval("XC") == "XC"
+    assert ri.extract_portion_to_eval("XIV") == "X"
 
 
 def test_split_into_portions_to_eval():
     assert ["I", "I", "I"] == ri.split_into_expanded_form("III")
-    assert ["L", "V", "I", "I", "I"] == ri.split_into_expanded_form("LVIII")
-    assert ["M", "CM", "XC", "IV"] == ri.split_into_expanded_form("MCMXCIV")
+    assert ["X", "IV"] == ri.split_into_expanded_form("XIV")
+    # assert ["L", "V", "I", "I", "I"] == ri.split_into_expanded_form("LVIII")
+    # assert ["M", "CM", "XC", "IV"] == ri.split_into_expanded_form("MCMXCIV")
 
 
 def test_numeral_to_integer():
